f19fff8d871f6222beca9221c3691c86feffa7fd
Vincent Heuschling <vhe@affini-tech.com>
2015-03-20 00:27:31 +0100

    Clustering

--- step02.scala
+++ step02.scala
@@ -18,3 +18,17 @@ dataAndLabel.take(1)
 //Array[(Array[Double], String)] = Array((Array(0.0, 215.0, 45076.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),normal.))
 
 
+
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.clustering._
+
+val vect = dataAndLabel.map(_._1)
+
+var res = new Array[Double](100);
+var x = 0
+
+for(i <- 2 to 10) {
+	val clusters = KMeans.train(vect, i, numIterations = 20)
+ 	res(x) = clusters.computeCost(vect); 
+ 	x = x+1;
+};
